d91b573c7beb8570899803da8c944fdf33fe6f24
TanawatTH <39165947+TanawatTH@users.noreply.github.com>
2025-03-17 09:00:00 +0000

    Write tests for whitelist and royalties

--- test/Zomboys.test.js
+++ test/Zomboys.test.js
@@ -24,4 +24,20 @@ describe("Zomboys", function () {
   it("Should not mint beyond max supply", async function () {
     // This would require minting 10000, but for test, assume it's checked
   });
+
+  it("Should allow presale mint with valid proof", async function () {
+    const mintPrice = ethers.utils.parseEther("0.05");
+    const leaf = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["address"], [addr1.address]));
+    const merkleRoot = ethers.utils.keccak256(leaf); // Simple root for one address
+    await zomboys.setMerkleRoot(merkleRoot);
+    await zomboys.togglePresale();
+
+    const proof = [leaf]; // Simplified
+    await zomboys.connect(addr1).presaleMint(proof, { value: mintPrice });
+    expect(await zomboys.ownerOf(1)).to.equal(addr1.address);
+  });
+
+  it("Should support ERC2981 royalties", async function () {
+    expect(await zomboys.supportsInterface("0x2a55205a")).to.equal(true); // ERC2981 interface
+  });
 });
